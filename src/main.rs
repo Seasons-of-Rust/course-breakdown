337b7214655bcec78bf028a297fa73fcebe8dab9
Forest Anderson <forestkzanderson@gmail.com>
2022-07-19 17:54:06 -0400

    Initial reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::fs::File;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Course {
+    code1: String,
+    code2: String,
+    code3: String,
+    code4: String,
+    section: String,
+    kind: String,
+    thing: String,
+    start: String,
+    end: String,
+    start_date: String,
+    end_date: String,
+    num1: String,
+    num2: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Load courses.csv
+    let file = File::open("courses.csv").unwrap();
+    let mut rdr = csv::Reader::from_reader(file);
+
+    // Get the vec ready
+    let mut courses: Vec<Course> = Vec::new();
+
+    for course in rdr.deserialize() {
+        courses.push(course.unwrap());
+    }
+
+    // Print courses
+    for course in courses {
+        dbg!(course);
+    }
 }
